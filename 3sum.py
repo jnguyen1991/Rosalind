8f0c958afe9c59fa455a07248f427c59401acd04
jnguyen1991 <jon.v.nguyen1991@gmail.com>
2018-04-27 10:57:53 -0700

    3sum
    turned to function to be able to return on finding a solution, based on wikipedia 3sum article

--- 3sum.py
+++ 3sum.py
@@ -16,34 +16,34 @@ def brute(line):
                     output = " ".join([str(x) for x in [a+1,b+1,c+1]])
                     return output
     return -1
+    
+def threesum(line):
+    lst = [int(x) for x in line.split()]
+    dct = {key:val for key,val in enumerate(lst)}
+    sort = sorted(dct, key=dct.get)
+    for i in range(len(sort)-2):
+        j = i+1
+        k = len(sort)-1
+        while j < k:
+            #print lst[sort[i]],lst[sort[j]],lst[sort[k]]
+            total = lst[sort[i]] + lst[sort[j]] + lst[sort[k]]
+            #print total
+            if total == 0:
+                output = " ".join([str(x) for x in sorted([sort[i]+1, sort[j]+1, sort[k]+1])])
+                return output
+            if total > 0:
+                k -= 1
+            else:
+                j += 1
+    return "-1"    
 
 if __name__ == "__main__":
-    fname = r"C:\Users\jnguyen3\Downloads\rosalind_3sum.txt"
+    fname = r"C:\Users\jnguyen3\Downloads\rosalind_3sum (1).txt"
     #brute force O(n^3)    
     with open(fname) as fhand:
         length = int(fhand.next().split()[1])
         for line in fhand:
-            a,b,c, = 0,0,0
-            lst = [int(x) for x in line.split()]
-            dct = {key:val for key,val in enumerate(lst)}
-            sort = sorted(dct, key=dct.get)
-            for i in range(len(sort)-2):
-                j = i+1
-                k = len(sort)-1
-                while j < k:
-                    #print lst[sort[i]],lst[sort[j]],lst[sort[k]]
-                    total = lst[sort[i]] + lst[sort[j]] + lst[sort[k]]
-                    #print total
-                    if total == 0:
-                        print i,j,k
-                        a,b,c = i,j,k
-                        break
-                    if total > 0:
-                        k -= 1
-                    else:
-                        j += 1
-            if a==b and b == c:
-                print -1
+            print threesum(line)
                 
             '''
             for i in range(length-2):
